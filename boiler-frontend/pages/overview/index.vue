5ed9b671df4e0d37edb2c10707964620e451d126
Justin Cho <beluga55@gmail.com>
2024-08-22 02:29:11 +0800

    Code Content Directly Here

--- boiler-frontend/pages/overview/index.vue
+++ boiler-frontend/pages/overview/index.vue
@@ -1,8 +1,30 @@
 <script setup>
- import Overview from '~/components/pages/overview/overview.vue'
+definePageMeta({
+  layout: 'overview',
+})
 </script>
 
 <template>
-  <Overview />
+  <div class="content__container">
+    <div class="about">
+      <h1>About the system</h1>
+    </div>
+  </div>
 </template>
 
+<style scoped lang="scss">
+.content__container {
+  padding: 18px 24px;
+
+  .about {
+    padding: 20px 18px;
+    background: var(--overview-secondary-background);
+    border-radius: 10px;
+
+    h1 {
+      font-size: 14px;
+      font-weight: 600;
+    }
+  }
+}
+</style>
