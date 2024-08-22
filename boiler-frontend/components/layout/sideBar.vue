b9ab061fe102c26c158c6684429a417b40d96a41
Justin Cho <beluga55@gmail.com>
2024-08-22 12:44:59 +0800

    Changed Lucide Icons to Radix Icons, and reapply styling

--- boiler-frontend/components/layout/sideBar.vue
+++ boiler-frontend/components/layout/sideBar.vue
@@ -2,17 +2,17 @@
 import { ref, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import DefaultImage from "@/assets/images/default-user-icon.jpg"
-import { ChevronLeft, Pen, Share2, Globe, Settings, LogIn as Logout } from 'lucide-vue-next'
-import ComboBox from '~/components/elements/ComboBox.vue'
+import { CaretLeftIcon, ReaderIcon, GlobeIcon, ShadowInnerIcon, Share1Icon, Pencil1Icon, GearIcon, EnterIcon } from '@radix-icons/vue'
+import ComboBox from '~/components/ui/combobox/ComboBox.vue'
 
 const route = useRoute()
 
 const navItems = ref([
-  { name: 'Overview', path: '/overview', icon: 'icon-overview' },
-  { name: 'Routes', path: '/routes', icon: Share2 },
-  { name: 'Revisions', path: '/revisions', icon: 'icon-circle' },
-  { name: 'Editor', path: '/editor', icon: Pen },
-  { name: 'Domains', path: '/domains', icon: Globe },
+  { name: 'Overview', path: '/overview', icon: ReaderIcon },
+  { name: 'Routes', path: '/routes', icon: Share1Icon },
+  { name: 'Revisions', path: '/revisions', icon: ShadowInnerIcon },
+  { name: 'Editor', path: '/editor', icon: Pencil1Icon },
+  { name: 'Domains', path: '/domains', icon: GlobeIcon },
 ])
 
 /* ====== ACTIVE CLASS BASED ON ROUTES ====== */
@@ -20,30 +20,22 @@ const isActive = computed(() => (path) => route.path === path)
 
 /* ====== RENDER ICON BASED ON TYPE ====== */
 const renderIcon = (icon) => {
-  if (typeof icon === 'string') {
-    if (icon === 'icon-circle') {
-      return h('span', { class: 'icon-circle text-[14px]' }, // ICOMOON return SVG to <span>
-          Array.from({ length: 11 }, (_, i) => h('span', { class: `path${i + 1}` }))
-      )
-    }
-    return h('span', { class: `${icon} text-[14px]` })
-  }
   return h(icon, { size: 14 })
 }
 </script>
 
 <template>
-  <aside class="w-[180px] h-screen flex flex-col justify-between border-r-2 border-solid border-overview-border">
+  <aside class="w-[180px] h-screen flex flex-col justify-between border-r-2 border-solid border-border">
     <div>
-      <header class="pb-2 border-b-2 border-solid border-overview-border">
+      <header class="pb-2 border-b-2 border-solid border-border">
         <div class="pt-3.5 px-3.5 flex items-center justify-between">
           <h1 class="text-base font-bold">BLDR.</h1>
-          <ChevronLeft class="cursor-pointer" :size="18" />
+          <CaretLeftIcon class="cursor-pointer w-[18px] h-[18px]"  />
         </div>
         <div class="credit-badge text-black mt-[7px] ml-3.5 rounded-[32px] text-center w-fit py-[3px] px-1.5">
           <p class="font-medium text-[10px]">100 credits remaining +</p>
         </div>
-        <div class="flex items-center gap-[7px] mt-3 mx-2 px-[13px] py-[9px] bg-overview-repo-button border-2 border-solid border-overview-border rounded-[6px]">
+        <div class="flex items-center gap-[7px] mt-3 mx-2 px-[13px] py-[9px] bg-background-secondary border-2 border-solid border-border rounded-[6px]">
           <ComboBox />
         </div>
       </header>
@@ -53,21 +45,21 @@ const renderIcon = (icon) => {
             :key="item.name"
             :to="item.path"
             class="py-2.5 px-[10.5px] flex items-center gap-2 text-xs font-medium w-full cursor-pointer no-underline text-inherit"
-            :class="{ 'bg-overview-button rounded-md text-overview-primary': isActive(item.path) }"
+            :class="{ 'bg-button rounded-md text-primary-system': isActive(item.path) }"
         >
           <component :is="renderIcon(item.icon)" />
           {{ item.name }}
         </NuxtLink>
       </nav>
     </div>
-    <footer class="border-t-2 border-solid border-overview-border py-[9px] px-2 flex items-center justify-between">
+    <footer class="border-t-2 border-solid border-border py-[9px] px-2 flex items-center justify-between">
       <div class="flex items-center gap-2">
         <img class="w-6 h-6" :src="DefaultImage" alt="Profile picture" />
         <p class="text-xs font-medium">ShaneWen</p>
       </div>
       <div class="flex items-center gap-3.5">
-        <Settings :size="12" />
-        <Logout :size="12" />
+        <GearIcon :size="12" />
+        <EnterIcon :size="12" />
       </div>
     </footer>
   </aside>
@@ -77,7 +69,7 @@ const renderIcon = (icon) => {
   @import url("@/assets/css/icon-font.css");
 
   .credit-badge {
-    background: linear-gradient(49deg, var(--overview-primary) 0%, var(--overview-secondary) 100%);
+    background: linear-gradient(49deg, #DCF928 0%, #EDFF7F 100%);
   }
 </style>
 
--- boiler-frontend/components/layout/topNav.vue
+++ boiler-frontend/components/layout/topNav.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-  import { Search } from "lucide-vue-next";
+  import { MagnifyingGlassIcon } from "@radix-icons/vue";
 </script>
 
 <template>
-  <nav class="bg-overview-secondary-background w-full">
+  <nav class="bg-background-secondary w-full">
     <div class="px-6 py-[11px] flex items-center">
-      <h1 class="text-sm font-bold text-overview-text-secondary flex-1">Pet Management System / Overview</h1>
+      <h1 class="text-sm font-bold text-text-secondary flex-1">Pet Management System / Overview</h1>
 
-      <div class="w-full bg-overview-border flex justify-center rounded-[5px] flex-1">
-        <button class="flex items-center gap-2.5 text-[10px] text-overview-text-secondary py-2">
-          <Search :size="12" />
+      <div class="w-full bg-border flex justify-center rounded-[5px] flex-1">
+        <button class="flex items-center gap-2.5 text-[10px] text-text-secondary py-2">
+          <MagnifyingGlassIcon :size="12" class="rotate-90" />
           Pet Management System
         </button>
       </div>
