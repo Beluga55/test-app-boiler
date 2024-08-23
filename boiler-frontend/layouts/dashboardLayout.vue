8fd4ac8ef6e942a05969e41b870f84bdc80315ac
unknown <beluga.github@gmail.com>
2024-08-23 15:15:42 +0800

    Pass toggleSidebar prompt to layout and also fix width scaling issue when close the sidebar

--- boiler-frontend/layouts/dashboardLayout.vue
+++ boiler-frontend/layouts/dashboardLayout.vue
@@ -1,12 +1,19 @@
 <script setup>
 import Sidebar from '~/components/layout/sideBar.vue'
 import TopNav from '~/components/layout/topNav.vue'
+import { ref } from 'vue'
+
+const isSidebarOpen = ref(true);
+
+const toggleSidebar = () => {
+  isSidebarOpen.value = !isSidebarOpen.value
+}
 </script>
 
 <template>
   <div class="flex">
-    <Sidebar />
-    <div class="w-[calc(100%-180px)]">
+    <Sidebar :isSidebarOpen="isSidebarOpen" @toggle-sidebar="toggleSidebar" />
+    <div :class="[isSidebarOpen ? 'w-[calc(100%-180px)]' : 'w-[calc(100%-55px)]']">
       <TopNav />
       <slot />
     </div>
