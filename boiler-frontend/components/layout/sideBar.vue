a8d542bca3db8f117f818636cfc56d38371882fe
unknown <beluga.github@gmail.com>
2024-08-23 15:13:29 +0800

    Added toggle sidebar functionality

--- boiler-frontend/components/layout/sideBar.vue
+++ boiler-frontend/components/layout/sideBar.vue
@@ -5,38 +5,58 @@ import DefaultImage from "~/assets/images/default-user-icon.jpg"
 import { CaretLeftIcon, ReaderIcon, GlobeIcon, ShadowInnerIcon, Share1Icon, Pencil1Icon, GearIcon, EnterIcon } from '@radix-icons/vue'
 import ComboBox from '~/components/ui/combobox/ComboBox.vue'
 
-const route = useRoute()
+const props = defineProps({
+  isSidebarOpen: Boolean
+})
 
-const navItems = ref([
+const emit = defineEmits(['toggle-sidebar'])
+
+const route = useRoute();
+const selectedRepository = ref('');
+
+const navItems = [
   { name: 'Overview', path: '/overview', icon: ReaderIcon },
   { name: 'Routes', path: '/routes', icon: Share1Icon },
   { name: 'Revisions', path: '/revisions', icon: ShadowInnerIcon },
   { name: 'Editor', path: '/editor', icon: Pencil1Icon },
   { name: 'Domains', path: '/domains', icon: GlobeIcon },
-])
+]
 
-/* ====== ACTIVE CLASS BASED ON ROUTES ====== */
+const repositories = [
+  { value: 'pets-management-system', label: 'pets-management-system' },
+  { value: 'e-commerce-system', label: 'e-commerce-system' },
+  { value: 'nuxt-tutorial', label: 'Nuxt-tutorial' },
+  { value: 'remix-tutorial', label: 'Remix-tutorial' },
+  { value: 'astro-tutorial', label: 'Astro-tutorial' },
+]
+
 const isActive = computed(() => (path) => route.path === path)
 
-/* ====== RENDER ICON BASED ON TYPE ====== */
-const renderIcon = (icon) => {
-  return h(icon, { size: 14 })
-}
+const toggleSidebar = () => emit('toggle-sidebar')
 </script>
 
 <template>
-  <aside class="w-[180px] h-screen flex flex-col justify-between border-r-2 border-solid border-border">
+  <aside :class="['h-screen flex flex-col justify-between border-r-2 border-solid border-border', props.isSidebarOpen ? 'w-[180px]' : 'w-auto']">
     <div>
       <header class="pb-2 border-b-2 border-solid border-border">
         <div class="pt-3.5 px-3.5 flex items-center justify-between">
-          <h1 class="text-base font-bold">BLDR.</h1>
-          <CaretLeftIcon class="cursor-pointer w-[18px] h-[18px]"  />
+          <h1 v-if="props.isSidebarOpen" class="text-base font-bold">BLDR.</h1>
+          <CaretLeftIcon
+              :class="['cursor-pointer', props.isSidebarOpen ? 'w-[18px] h-[18px]' : 'w-[23px] h-[23px]']"
+              @click="toggleSidebar"
+          />
         </div>
-        <div class="credit-badge text-black mt-[7px] ml-3.5 rounded-[32px] text-center w-fit py-[3px] px-1.5">
+        <div v-if="props.isSidebarOpen" class="credit-badge text-black mt-[7px] ml-3.5 rounded-[32px] text-center w-fit py-[3px] px-1.5">
           <p class="font-medium text-[10px]">100 credits remaining +</p>
         </div>
-        <div class="flex items-center gap-[7px] mt-3 mx-2 px-[13px] py-[9px] bg-background-secondary border-2 border-solid border-border rounded-[6px]">
-          <ComboBox />
+        <div v-if="props.isSidebarOpen" class="flex items-center gap-[7px] mt-3 mx-2 px-[13px] py-[9px] bg-button-secondary border-2 border-solid border-border rounded-[6px]">
+          <ComboBox
+              v-model="selectedRepository"
+              :items="repositories"
+              placeholder="Select Repository..."
+              popover-class="ml-2.5"
+              :command-style="{ background: '#353535' }"
+          />
         </div>
       </header>
       <nav class="px-2 py-1">
@@ -47,17 +67,17 @@ const renderIcon = (icon) => {
             class="py-2.5 px-[10.5px] flex items-center gap-2 text-xs font-medium w-full cursor-pointer no-underline text-inherit"
             :class="{ 'bg-button rounded-md text-primary-system': isActive(item.path) }"
         >
-          <component :is="renderIcon(item.icon)" />
-          {{ item.name }}
+          <component :is="item.icon" :size="14" />
+          <span v-if="props.isSidebarOpen">{{ item.name }}</span>
         </NuxtLink>
       </nav>
     </div>
-    <footer class="border-t-2 border-solid border-border py-[9px] px-2 flex items-center justify-between">
+    <footer :class="['border-t-2 border-solid border-border py-[9px] px-2 flex items-center', props.isSidebarOpen ? 'justify-between' : 'justify-center']">
       <div class="flex items-center gap-2">
         <img class="w-6 h-6" :src="DefaultImage" alt="Profile picture" />
-        <p class="text-xs font-medium">ShaneWen</p>
+        <p v-if="props.isSidebarOpen" class="text-xs font-medium">ShaneWen</p>
       </div>
-      <div class="flex items-center gap-3.5">
+      <div v-if="props.isSidebarOpen" class="flex items-center gap-3.5">
         <GearIcon :size="12" />
         <EnterIcon :size="12" />
       </div>
@@ -66,10 +86,9 @@ const renderIcon = (icon) => {
 </template>
 
 <style lang="scss" scoped>
-  @import url("@/assets/css/icon-font.css");
+@import url("@/assets/css/icon-font.css");
 
-  .credit-badge {
-    background: linear-gradient(49deg, #DCF928 0%, #EDFF7F 100%);
-  }
+.credit-badge {
+  background: linear-gradient(49deg, #DCF928 0%, #EDFF7F 100%);
+}
 </style>
-
