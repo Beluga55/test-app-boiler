f0f22635aa8c39ec552fd5a4249d76b9edced7aa
Justin Cho <beluga55@gmail.com>
2024-08-26 11:06:03 +0800

    Added Revisions With Data

--- boiler-frontend/pages/overview/index.vue
+++ boiler-frontend/pages/overview/index.vue
@@ -44,12 +44,52 @@ const featureData = ref([
     description: "This feature that allows both students and instructors to track academic progress throughout the course"
   },
 ])
+
+/* ====== OPEN REVISIONS DATA ====== */
+const openRevisionsData = ref([
+  {
+    name: "Assignment Module",
+    status: "Ready"
+  },
+  {
+    name: "Assignment Module",
+    status: "Ready"
+  },
+  {
+    name: "Assignment Module",
+    status: "Ready"
+  },
+  {
+    name: "Assignment Module",
+    status: "Ready"
+  },
+  {
+    name: "Assignment Module",
+    status: "Ready"
+  },
+  {
+    name: "Assignment Module",
+    status: "Ready"
+  },
+  {
+    name: "Assignment Module",
+    status: "Ready"
+  },
+  {
+    name: "Assignment Module",
+    status: "Ready"
+  },
+]);
+
+/* ====== TO TEST EMPTY STATE (Uncomment) ====== */
+// openRevisionsData.value = [];
+
 </script>
 
 <template>
   <div class="py-[18px] px-6 h-[calc(100vh-3.7rem)] overflow-auto">
     <div class="py-5 px-[18px] bg-background-secondary rounded-[10px] mb-[18px] border-border border-2">
-      <h1 class="text-[14px] font-semibold">About the system</h1>
+      <h1 class="text-[14px] font-bold">About the system</h1>
       <p class="text-xs font-medium mt-3">This system helps pet care businesses like veterinary clinics and grooming services run more smoothly. It
         includes tools for scheduling appointments, managing medical records, tracking service history, and
         communicating with customers. These features enhance operational efficiency and improve client service.</p>
@@ -66,14 +106,25 @@ const featureData = ref([
       </div>
     </div>
 
-    <div class="open-revision">
+    <div>
       <h1 class="font-medium text-[14px]">Open Revisions</h1>
       <div class="mt-2.5 flex gap-4">
-        <div class="flex-1 h-[225px] flex flex-col items-center justify-center border-dashed border-2 border-border-secondary rounded-[10px]">
-          <p class="text-[14px]">There aren't any new feature being developed.</p>
-          <p class="text-[14px] mt-2.5">Go to <NuxtLink :to="'/revisions'" class="text-primary-system">Revisions</NuxtLink> to create a new one.</p>
+        <div v-if="openRevisionsData.length === 0" class="flex-1 h-[225px] flex flex-col items-center justify-center border-dashed border-2 border-border-secondary rounded-[10px]">
+          <p class="text-[14px] text-center">There aren't any new features being developed.</p>
+          <p class="text-[14px] text-center mt-2.5">
+            Go to <NuxtLink :to="'/revisions'" class="text-primary-system">Revisions</NuxtLink> to create a new one.
+          </p>
         </div>
-        <div class="rounded-[10px] bg-secondary-system w-[225px] h-[225px] px-5 py-5 relative overflow-hidden z-[1]">
+        <div v-else class="max-h-[225px] overflow-y-scroll w-full flex flex-col gap-2.5">
+            <div v-for="(revision, index) in openRevisionsData" :key="index" class="bg-background-secondary rounded-[10px] px-[9px] py-2.5">
+              <div class="flex items-center">
+                <div class="w-[20px] h-[20px] rounded-full bg-primary-system border-border-status border-2 border-solid mr-2.5"></div>
+                <span class="text-xs font-medium">{{ revision.name }}</span>
+                <span class="ml-auto px-2.5 text-xs font-medium text-primary-system">{{ revision.status }}</span>
+              </div>
+            </div>
+        </div>
+        <div class="rounded-[10px] bg-secondary-system min-w-[225px] h-[225px] px-5 py-5 relative overflow-hidden z-[1]">
           <img src="~/assets/images/hash.svg" alt="hash" class="hash absolute inset-0 w-[190px] h-[190px] object-cover z-0 opacity-10 rotate-[28deg] left-[-50px] top-[75px]"/>
           <div class="relative z-10">
             <h1 class="font-black text-[21px] text-background-secondary">Ready to <br/>launch?</h1>
@@ -109,24 +160,5 @@ const featureData = ref([
     background: #f1f1f1;
     border-radius: 10px;
   }
-
-  /* grid 1 */
-
-  /*position: absolute;*/
-  /*width: 170.67px;*/
-  /*height: 167.95px;*/
-  /*left: 10.85px;*/
-  /*top: 45px;*/
-
-  /*background: url(grid.png);*/
-  /*transform: rotate(28deg);*/
-
-  /*!* Inside auto layout *!*/
-  /*flex: none;*/
-  /*order: 2;*/
-  /*flex-grow: 0;*/
-  /*z-index: 2;*/
-
-
 </style>
 
