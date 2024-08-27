7d01de114e5ef84ae86210d493499c28e8ff914b
Justin Cho <beluga55@gmail.com>
2024-08-28 01:06:21 +0800

    Added New Files

--- boiler-frontend/layouts/dashboardLayout.vue
+++ boiler-frontend/layouts/dashboardLayout.vue
@@ -1,18 +1,39 @@
 <script setup>
 import Sidebar from '~/components/layout/sideBar.vue'
 import TopNav from '~/components/layout/topNav.vue'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const isSidebarOpen = ref(true);
 const selectedRepository = ref('');
+const isAndroid = ref(false);
+const isIOS = ref(false);
 
 const toggleSidebar = () => {
   isSidebarOpen.value = !isSidebarOpen.value
 }
+
+const detectOS = () => {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/android/i.test(userAgent)) {
+    isAndroid.value = true;
+  }
+
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    isIOS.value = true;
+  }
+}
+
+onMounted(() => {
+  detectOS();
+})
 </script>
 
 <template>
-  <div class="flex">
+  <div v-if="isAndroid || isIOS">
+    
+  </div>
+  <div v-else class="flex">
     <Sidebar :isSidebarOpen="isSidebarOpen" @toggle-sidebar="toggleSidebar" @update:selectedRepository="selectedRepository = $event" />
     <div :class="[isSidebarOpen ? 'w-[calc(100%-242px)]' : 'w-[calc(100%-55px)]']">
       <TopNav :selectedRepository="selectedRepository" />
