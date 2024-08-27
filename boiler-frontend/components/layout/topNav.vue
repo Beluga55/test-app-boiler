a6cf26ec05929a3d555b17bb00be2300d505672c
Justin Cho <beluga55@gmail.com>
2024-08-27 15:58:07 +0800

    Using Dynamic Breadcrumb by passing props value at sidebar button

--- boiler-frontend/components/layout/topNav.vue
+++ boiler-frontend/components/layout/topNav.vue
@@ -1,6 +1,14 @@
 <script lang="ts" setup>
 import {MagnifyingGlassIcon} from "@radix-icons/vue";
 import {ref} from "vue";
+import { useRoute } from "vue-router";
+
+const props = defineProps({
+  selectedRepository: {
+    type: String,
+    default: 'Pet Management System'
+  }
+});
 
 const staging = [
   {value: "Staging v0.1", label: "Staging v0.1"},
@@ -12,18 +20,32 @@ const staging = [
 
 const selectedStaging = ref('');
 const route = useRoute();
+
+const capitalizeFirstLetter = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const dynamicTitle = computed(() => {
+  const routeName = route.name ? capitalizeFirstLetter(route.name.toString()) : '';
+
+  if (!props.selectedRepository) {
+    return routeName;
+  }
+
+  return `${props.selectedRepository} / ${routeName}`;
+});
 </script>
 
 <template>
   <nav class="bg-background-secondary w-full border-border border-b-2">
-    <div class="px-6 py-[10px] flex items-center gap-6">
-      <h1 class="text-[14px] font-bold text-text-secondary flex-1">Pet Management System / Overview</h1>
+    <div class="px-6 py-2 flex items-center gap-6">
+      <h1 class="text-[14px] font-bold text-text-secondary flex-1">{{ dynamicTitle }}</h1>
 
       <div
           class="max-w-[450px] bg-button-secondary flex justify-center rounded-[5px] flex-1 border-2 border-solid border-border">
         <button class="flex items-center gap-2.5 text-[14px] text-text-secondary py-2">
           <MagnifyingGlassIcon :size="14" class="rotate-90"/>
-          Pet Management System
+          {{ props.selectedRepository || 'Search' }}
         </button>
       </div>
 
